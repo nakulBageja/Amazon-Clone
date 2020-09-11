956f3ef258ab530abd09083827cdf184826c3716
unknown <nbageja@gmail.com>
2020-09-11 17:22:30 +0530

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Home />
+      </div>
+    </Router>
   );
 }
 
